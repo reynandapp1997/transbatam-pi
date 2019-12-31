52afc5ebeaafc917cd49976c39b76a913e52d5e3
Reynanda Putra Pratama <reynandapp1997@gmail.com>
2019-12-31 16:46:05 +0700

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from requests.exceptions import HTTPError
 import serial
 import string
 import pynmea2
-import threading
+from threading import Thread
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -15,14 +15,11 @@ led_red_pin = 18
 led_green_pin = 23
 button_pin = 17
 
-port="/dev/ttyAMA0"
-ser=serial.Serial(port, baudrate=9600, timeout=0.5)
-
 GPIO.setup(led_red_pin, GPIO.OUT)
 GPIO.setup(led_green_pin, GPIO.OUT)
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def listenButtonPressed(state_pressed):
+def changeState(state_pressed):
 	global state
 	if state_pressed == False:
 		print "pressed"
@@ -48,37 +45,43 @@ def sendCoordinates(lat, long):
 			print "Berhasil mengirim koordinat"
 		else:
 			print "Tidak berhasil mengirim koordinat"
+	time.sleep(5)
 
 def listenState():
-	threading.Timer(5.0, listenState).start()
-	global state
-	global ser
-	if state == True:
-		GPIO.output(led_green_pin, 1)
-		GPIO.output(led_red_pin, 0)
+	while True:
+		global state
+		if state == True:
+			GPIO.output(led_green_pin, 1)
+			GPIO.output(led_red_pin, 0)
 
-		dataout = pynmea2.NMEAStreamReader()
-		newdata=ser.readline()
+		port="/dev/ttyAMA0"
+		ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+			dataout = pynmea2.NMEAStreamReader()
+			newdata=ser.readline()
 
-		if newdata[0:6] == "$GPRMC":
-			newmsg=pynmea2.parse(newdata)
-			lat=newmsg.latitude
-			lng=newmsg.longitude
-			print lat
-			print lng
-			sendCoordinates(lat, lng)
-	else:
-		GPIO.output(led_red_pin, 1)
-		GPIO.output(led_green_pin, 0)
+			if newdata[0:6] == "$GPRMC":
+				newmsg=pynmea2.parse(newdata)
+				lat=newmsg.latitude
+				lng=newmsg.longitude
+				print lat
+				print lng
+				sendCoordinates(lat, lng)
+		else:
+			GPIO.output(led_red_pin, 1)
+			GPIO.output(led_green_pin, 0)
 
-listenState()
-
-try:
-	print 'START'
+def listenButtonPressed():
 	while True:
 		button_state = GPIO.input(button_pin)
-		listenButtonPressed(button_state)
+		changeState(button_state)
 
-except KeyboardInterrupt:
-	print 'END'
-	GPIO.cleanup()
+Thread(target = listenButtonPressed).start()
+Thread(target = listenState).start()
+
+# try:
+# 	print 'START'
+# 	while True:
+
+# except KeyboardInterrupt:
+# 	print 'END'
+# 	GPIO.cleanup()
